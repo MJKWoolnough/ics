0529f51fd707da6f8eb986edaf7f725d8db5bd7b
Michael Woolnough <michael.woolnough@gmail.com>
2015-05-07 13:26:25 +0100

    all identifiers are not exported

--- components.go
+++ components.go
@@ -1,36 +1,81 @@
 package ics
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
+
+type componentType int
 
 type component interface {
-	setAttribute(token, []token) error
-	setValue(token) error
+	setAttribute(attribute) error
+	setValue(string) error
 }
 
 func componentFromToken(t token) component {
 	switch strings.ToUpper(t.data) {
+	case "BEGIN":
+		return new(beginComponent)
+	case "END":
+		return new(endComponent)
 	default:
-		return &UnknownComponent{
+		return &unknownComponent{
 			Name: t.data,
 		}
 	}
 }
 
-type UnknownComponent struct {
-	Name, Value string
-	Attributes  []Attribute
+const (
+	beVCalendar = "VCALENDAR"
+	beVEvent    = "VEVENT"
+	beVTodo     = "VTODO"
+	beVJournal  = "VJOURNAL"
+	beVFreeBusy = "VFREEBUSY"
+	beVTimezone = "VTIMEZONE"
+	beStandard  = "STANDARD"
+	beDaylight  = "DAYLIGHT"
+	beVAlarm    = "VALARM"
+)
+
+type beginComponent string
+
+func (beginComponent) setAttribute(_ attribute) error {
+	return ErrNoAttributes
 }
 
-func (u *UnknownComponent) setAttribute(pn token, pvs []token) error {
-	attr, err := attributeFromTokens(pn, pvs)
-	if err != nil {
-		return err
-	}
+func (b *beginComponent) setValue(v string) error {
+	*b = beginComponent(v)
+	return nil
+}
+
+type endComponent string
+
+func (endComponent) setAttribute(_ attribute) error {
+	return ErrNoAttributes
+}
+
+func (e *endComponent) setValue(v string) error {
+	*e = endComponent(v)
+	return nil
+}
+
+type unknownComponent struct {
+	Name, Value string
+	Attributes  []attribute
+}
+
+func (u *unknownComponent) setAttribute(attr attribute) error {
 	u.Attributes = append(u.Attributes, attr)
 	return nil
 }
 
-func (u *UnknownComponent) setValue(v token) error {
-	u.Value = v.data
+func (u *unknownComponent) setValue(v string) error {
+	u.Value = v
 	return nil
 }
+
+// Errors
+
+var (
+	ErrNoAttributes = errors.New("component doesn't allow attributes")
+)
