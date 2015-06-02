d1fa35ed800b6f11b9f20b37dfe1c63550ece409
Michael Woolnough <michael.woolnough@gmail.com>
2015-06-02 21:21:12 +0100

    added Validate and Data methods to summary properties

--- propertiesSummary.go
+++ propertiesSummary.go
@@ -46,6 +46,26 @@ func (p *parser) readAttachProperty() (property, error) {
 	}, nil
 }
 
+func (a attach) Validate() bool {
+	return true
+}
+
+func (a attach) Data() propertyData {
+	params := make(map[string]attribute)
+	if a.Mime != "" {
+		params[fmttypeparam] = fmtType(a.Mime)
+	}
+	if !a.URI {
+		params[encodingparam] = encodingBase64
+		params[valuetypeparam] = valueBinary
+	}
+	return propertyData{
+		Name:   attachp,
+		Params: params,
+		Value:  string(a.Data),
+	}
+}
+
 type categories struct {
 	Language   string
 	Categories []string
@@ -68,6 +88,29 @@ func (p *parser) readCategoriesProperty() (property, error) {
 		language,
 		textSplit(v, ','),
 	}, nil
+}
+
+func (c categories) Validate() bool {
+	return true
+}
+
+func (c categories) Data() propertyData {
+	params := make(map[string]params)
+	if c.Language != "" {
+		params[languageparam] = language(c.Language)
+	}
+	val := make([]byte, 0, 1024)
+	for n, cat := range c.Categories {
+		if n > 0 {
+			val = append(val, ',')
+		}
+		val = append(val, escape(cat)...)
+	}
+	return propertyData{
+		Name:   categoriesp,
+		Params: params,
+		Value:  string(val),
+	}
 }
 
 const (
@@ -95,54 +138,60 @@ func (p *parser) readClassProperty() (property, error) {
 	}
 }
 
-type comment altrepLanguageData
-
-func (p *parser) readCommentProperty() (property, error) {
-	as, err := p.readAttributes(altrepparam, languageparam)
-	if err != nil {
-		return nil, err
+func (c class) String() string {
+	switch c {
+	case classPublic:
+		return "PUBLIC"
+	case classPrivate:
+		return "PRIVATE"
+	case classConfidential:
+		return "CONFIDENTIAL"
+	default:
+		return "PRIVATE"
 	}
-	var altRep, languageStr string
-	if alt, ok := as[altrepparam]; ok {
-		altRep = string(alt.(altrep))
-	}
-	if l, ok := as[languageparam]; ok {
-		languageStr = string(l.(language))
-	}
-	v, err := p.readValue()
-	if err != nil {
-		return nil, err
-	}
-	return comment{
-		altRep,
-		languageStr,
-		string(unescape(v)),
-	}, nil
 }
 
-type description altrepLanguageData
+func (c class) Validate() bool {
+	return c == classPublic || c == classPrivate || c == classConfidential
+}
+
+func (c class) Data() propertyData {
+	return propertyData{
+		Name:  classp,
+		Value: c.String(),
+	}
+}
+
+type comment struct {
+	altrepLanguageData
+}
+
+func (p *parser) readCommentProperty() (property, error) {
+	a, err := p.readAltrepLanguageData()
+	if err != nil {
+		return nil, err
+	}
+	return comment{a}, nil
+}
+
+func (c comment) Data() propertyData {
+	return c.data(commentp)
+}
+
+type description struct {
+	altrepLanguageData
+}
 
 func (p *parser) readDescriptionProperty() (property, error) {
-	as, err := p.readAttributes(altrepparam, languageparam)
+	a, err := p.readAltrepLanguageData()
 	if err != nil {
 		return nil, err
 	}
-	var altRep, languageStr string
-	if alt, ok := as[altrepparam]; ok {
-		altRep = string(alt.(altrep))
-	}
-	if l, ok := as[languageparam]; ok {
-		languageStr = string(l.(language))
-	}
-	v, err := p.readValue()
-	if err != nil {
-		return nil, err
-	}
-	return description{
-		altRep,
-		languageStr,
-		string(unescape(v)),
-	}, nil
+	return description{a}, nil
+}
+
+func (d description) Data() propertyData {
+	return c.data(descriptionp)
 }
 
 type geo struct {
@@ -169,29 +218,31 @@ func (p *parser) readGeoProperty() (property, error) {
 	return geo{la, lo}, nil
 }
 
-type location altrepLanguageData
+func (g geo) Validate() bool {
+	return true
+}
+
+func (g geo) Data() propertyData {
+	return propertyData{
+		Name:  geop,
+		Value: strconv.FormatFloat(g.Latitude, 'f', -1, 64) + ";" + strconv.FormatFloat(g.Longitude, 'f', -1, 64),
+	}
+}
+
+type location struct {
+	altrepLanguageData
+}
 
 func (p *parser) readLocationProperty() (property, error) {
-	as, err := p.readAttributes(altrepparam, languageparam)
+	a, err := p.readAltrepLanguageData()
 	if err != nil {
 		return nil, err
 	}
-	var altRep, languageStr string
-	if alt, ok := as[altrepparam]; ok {
-		altRep = string(alt.(altrep))
-	}
-	if l, ok := as[languageparam]; ok {
-		languageStr = string(l.(language))
-	}
-	v, err := p.readValue()
-	if err != nil {
-		return nil, err
-	}
-	return location{
-		altRep,
-		languageStr,
-		string(unescape(v)),
-	}, nil
+	return location{a}, nil
+}
+
+func (l location) Data() propertyData {
+	return c.data(locationp)
 }
 
 type percentComplete int
@@ -211,6 +262,17 @@ func (p *parser) readPercentCompleteProperty() (property, error) {
 	return percentComplete(pc), nil
 }
 
+func (p percentComplete) Validate() bool {
+	return p >= 0 && p <= 100
+}
+
+func (p percentComplete) Data() propertyData {
+	return propertyData{
+		Name:  percentcompletep,
+		Value: strconv.Itoa(int(p)),
+	}
+}
+
 type priority int
 
 func (p *parser) readPriorityProperty() (property, error) {
@@ -226,6 +288,17 @@ func (p *parser) readPriorityProperty() (property, error) {
 		return nil, ErrUnsupportedValue
 	}
 	return priority(pc), nil
+}
+
+func (p priority) Validate() bool {
+	return p >= 0 && p <= 9
+}
+
+func (p percentComplete) Data() propertyData {
+	return propertyData{
+		Name:  priorityp,
+		Value: strconv.Itoa(int(p)),
+	}
 }
 
 type resources struct {
@@ -254,6 +327,32 @@ func (p *parser) readResourcesProperty() (property, error) {
 		languageStr,
 		textSplit(v, ','),
 	}, nil
+}
+
+func (r resources) Validate() bool {
+	return true
+}
+
+func (r resources) Data() propertyData {
+	params := make(map[string]attributes)
+	if a.AltRep != "" {
+		params[altrepparam] = altrep(a.AltRep)
+	}
+	if a.Language != "" {
+		params[languageparam] = language(a.Language)
+	}
+	val := make([]byte, 0, 1024)
+	for n, res := range r.Resources {
+		if n > 0 {
+			val = append(val, ',')
+		}
+		val = append(val, encode(res)...)
+	}
+	return propertyData{
+		Name:   resourcesp,
+		Params: params,
+		Value:  string(val),
+	}
 }
 
 const (
@@ -296,27 +395,48 @@ func (p *parser) readStatusProperty() (property, error) {
 	}
 }
 
+func (s status) String() string {
+	switch s {
+	case statusTentative:
+		return "TENTATIVE"
+	case statusConfirmed:
+		return "CONFIRMED"
+	case statusNeedsAction:
+		return "NEED-ACTION"
+	case statusCompleted:
+		return "COMPLETED"
+	case statusInProgress:
+		return "IN-PROGRESS"
+	case statusDraft:
+		return "DRAFT"
+	case statusFinal:
+		return "FINAL"
+	case statusCancelled:
+		return "CANCELLED"
+	}
+}
+
+func (s status) Validate() bool {
+	return s >= statusTentative && s <= statusCancelled
+}
+
+func (s status) Data() propertyData {
+	return propertyData{
+		Name:  statusp,
+		Value: s.String(),
+	}
+}
+
 type summary altrepLanguageData
 
 func (p *parser) readSummaryProperty() (property, error) {
-	as, err := p.readAttributes(altrepparam, languageparam)
+	a, err := p.readAltrepLanguageData()
 	if err != nil {
 		return nil, err
 	}
-	var altRep, languageStr string
-	if alt, ok := as[altrepparam]; ok {
-		altRep = string(alt.(altrep))
-	}
-	if l, ok := as[languageparam]; ok {
-		languageStr = string(l.(language))
-	}
-	v, err := p.readValue()
-	if err != nil {
-		return nil, err
-	}
-	return summary{
-		altRep,
-		languageStr,
-		string(unescape(v)),
-	}, nil
+	return summary{a}, nil
+}
+
+func (s summary) Data() propertyData {
+	return c.data(summaryp)
 }
