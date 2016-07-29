5ef42ec19d2978760d793697a70e21babfb2e12d
Michael Woolnough <michael.woolnough@gmail.com>
2016-07-29 09:12:36 +0100

    modified testing layout and added tests for Boolean

--- types_test.go
+++ types_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -13,10 +14,11 @@ type typeTest struct {
 	Input  interface {
 		Decode(map[string]string, string) error
 	}
-	Output interface {
+	Match interface {
 		Encode(io.Writer)
 	}
-	Error error
+	Output string
+	Error  error
 }
 
 func testType(t *testing.T, tests []typeTest) {
@@ -24,32 +26,67 @@ func testType(t *testing.T, tests []typeTest) {
 	for n, test := range tests {
 		err := test.Input.Decode(test.Params, test.Data)
 		if err != test.Error {
-			t.Errorf("test %d: expecting error %s, got %s", n+1, test.Error, err)
+			if !reflect.DeepEqual(err, test.Error) {
+				t.Errorf("test %d: expecting error %s, got %s", n+1, test.Error, err)
+				continue
+			}
+		}
+		if !reflect.DeepEqual(test.Input, test.Match) {
+			t.Errorf("test %d: input does not match expected", n+1)
 			continue
 		}
-		if !reflect.DeepEqual(test.Input, test.Output) {
-			t.Errorf("test %d: input and output do not match", n+1)
-			continue
+		test.Match.Encode(&buf)
+		if str := buf.String(); str != test.Output {
+			t.Errorf("test %d: expecting output string %q, got %q", n+1, test.Output, str)
 		}
-		test.Output.Encode(&buf)
-		if str := buf.String(); str != test.Data {
-			t.Errorf("test %d: expecting output string %q, got %q", test.Data, str)
-		}
+		buf.Reset()
 	}
 }
 
 func TestBinary(t *testing.T) {
 	testType(t, []typeTest{
 		{
-			Input:  &Binary{},
-			Output: &Binary{},
-			Error:  ErrInvalidEncoding,
+			Input: &Binary{},
+			Match: &Binary{},
+			Error: ErrInvalidEncoding,
 		},
 		{
 			Params: map[string]string{"ENCODING": "BASE64"},
 			Data:   "MTIzNDU=",
 			Input:  &Binary{},
-			Output: &Binary{'1', '2', '3', '4', '5'},
+			Match:  &Binary{'1', '2', '3', '4', '5'},
+			Output: "MTIzNDU=",
+		},
+	})
+}
+
+func TestBoolean(t *testing.T) {
+	tr := new(Boolean)
+	fa := new(Boolean)
+	*tr = true
+	testType(t, []typeTest{
+		{
+			Data:   "False",
+			Input:  fa,
+			Match:  fa,
+			Output: "FALSE",
+		},
+		{
+			Data:   "true",
+			Input:  tr,
+			Match:  tr,
+			Output: "TRUE",
+		},
+		{
+			Data:   "HotDog",
+			Input:  fa,
+			Match:  fa,
+			Output: "FALSE",
+			Error: &strconv.NumError{
+				Func: "ParseBool",
+				Num:  "HotDog",
+				Err:  strconv.ErrSyntax,
+			},
 		},
 	})
 }
