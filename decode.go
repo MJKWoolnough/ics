d81c303035da53536fa31404e6ee207969e7a110
Michael Woolnough <michael.woolnough@gmail.com>
2016-08-18 14:30:55 +0100

    corrected Decode to check the initial line itself

--- decode.go
+++ decode.go
@@ -1,6 +1,9 @@
 package ics
 
-import "io"
+import (
+	"io"
+	"strings"
+)
 
 type section interface {
 	decode(tokeniser) error
@@ -10,8 +13,19 @@ type section interface {
 
 // Decode decodes an iCalendar object from the given reader
 func Decode(r io.Reader) (*Calendar, error) {
+	t := newTokeniser(&unfolder{r: r})
+	if p, err := t.GetPhrase(); err != nil {
+		return nil, err
+	} else if p.Type != phraseContentLine {
+		if t.Err == io.EOF {
+			return nil, io.ErrUnexpectedEOF
+		}
+		return nil, t.Err
+	} else if strings.ToUpper(p.Data[0].Data) != "BEGIN" || strings.ToUpper(p.Data[len(p.Data)-1].Data) != "VCALENDAR" {
+		return nil, ErrInvalidCalendar
+	}
 	cal := new(Calendar)
-	if err := cal.decode(newTokeniser(&unfolder{r: r})); err != nil {
+	if err := cal.decode(t); err != nil {
 		return nil, err
 	}
 	return cal, nil
